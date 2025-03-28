8fae91b2f193f401d4d4097bc07682c5215dfdda
Jherson <jherson.lopez88@gmail.com>
2025-03-28 01:54:23 -0500

    fix: type params changed in  productEdit page

--- app/product/[id]/edit/page.tsx
+++ app/product/[id]/edit/page.tsx
@@ -1,15 +1,16 @@
 import CreateForm from '@/app/_components/CreateForm'
 import { getProductById } from '@/app/actions/products-actions';
+import { Params } from 'next/dist/server/request/params';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
 export default async function page({
     params,
 }: {
-    params: { id: string }
+    params: Params
 }) {
     const { id } = await params;
-    const product = await getProductById(id);
+    const product = await getProductById(id as string);
     if (!product) {
         redirect('/');
     }
